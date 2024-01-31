4c752dfc94742192316cc58de7e94988de6f89bd
SiddhiShah03 <109909860+SiddhiShah03@users.noreply.github.com>
2024-01-31 22:13:03 +0530

    Add files via upload

---
+++ PrintingPositiveNumbers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1=[12,-7,5,64,-14]
+pn=[]
+for num in list1:
+    if num>0:
+        pn.append(num)
+print("Output:", ",".join(map(str,pn)))
+
+
+# In[2]:
+
+
+list2=[12,14,-95,3]
+pn2=[]
+for num in list1:
+    if num>0:
+        pn2.append(num)
+print("Output:", pn2)
+
+
+# In[ ]:
+
+
+
+
