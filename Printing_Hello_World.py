69451577d0a32d76cf0ec441534864d21a124b80
SiddhiShah03 <109909860+SiddhiShah03@users.noreply.github.com>
2024-01-31 20:00:27 +0530

    Add files via upload

---
+++ Printing_Hello_World.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Displaying "Hello World!" on the screen 
+
+
+# In[2]:
+
+
+#print is a Python defined default function, we are simply using it here
+
+
+# In[3]:
+
+
+print("Hello World!")
+
